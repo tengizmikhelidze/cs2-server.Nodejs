7408a45f1d07942ab9dbee25b8b61cc3e6a5fa72
10GO <mixelidzet@gmail.com>
2025-02-26 01:05:31 +0400

    feat - cors

--- src/app.mjs
+++ src/app.mjs
@@ -1,7 +1,15 @@
 import express from 'express';
 import './cs2/cs2-servers.mjs'
+import cors from "cors";
 
 const app = express();
+
+app.use(cors({
+    origin: 'http://localhost:4200',  // Allow only your frontend app
+    methods: ['GET', 'POST'],        // Allow only GET and POST methods (if needed)
+    allowedHeaders: ['Content-Type'], // Allow specific headers (if needed)
+}));
+
 app.use(express.json());
 
 export default app;
--- src/cs2/cs2-servers.mjs
+++ src/cs2/cs2-servers.mjs
@@ -1,7 +1,6 @@
 // src/server.mjs
 import express from 'express';
 import SteamServerQuery from 'steam-server-query'; // Import the package
-import cors from 'cors';
 
 const app = express();
 const port = 3000;
@@ -9,10 +8,6 @@ const port = 3000;
 // Replace with your CS2 server IP and port
 const SERVER_IP = '5.189.166.19'; // e.g., '123.45.67.89'
 const SERVER_PORT = 5555; // Default port for CS servers
-
-app.use(cors({
-    origin: 'http://localhost:4200', // Adjust to the correct origin
-}));
 
 app.get('/cs2/servers', (req, res) => {
     const query = new SteamServerQuery(SERVER_IP, SERVER_PORT);
