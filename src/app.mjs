ad66db4afb23a8048feb12003282a434ff90fb74
10GO <mixelidzet@gmail.com>
2025-02-26 01:18:10 +0400

    fix - cors

--- src/app.mjs
+++ src/app.mjs
@@ -1,15 +1,29 @@
 import express from 'express';
-import './cs2/cs2-servers.mjs'
 import cors from "cors";
+import SteamServerQuery from "steam-server-query";
 
 const app = express();
+const SERVER_IP = '5.189.166.19'; // e.g., '123.45.67.89'
+const SERVER_PORT = 5555; // Default port for CS servers
 
 app.use(cors({
-    origin: '*',  // Allow only your frontend app
+    origin: 'http://localhost:4200',  // Allow only your frontend app
     methods: ['GET', 'POST'],        // Allow only GET and POST methods (if needed)
     allowedHeaders: ['Content-Type'], // Allow specific headers (if needed)
 }));
 
 app.use(express.json());
+
+app.get('/cs2/servers', (req, res) => {
+    const query = new SteamServerQuery(SERVER_IP, SERVER_PORT);
+
+    query.getInfo()
+        .then(info => {
+            res.json(info);
+        })
+        .catch(error => {
+            console.error(error);
+        });
+});
 
 export default app;
